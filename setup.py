d334c39cffc3b2f1fdaa375c06b4b6877b3afccb
Eric Langlois <eric@langlois.xyz>
2018-05-12 01:20:10 -0400

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Package setup script."""
+import setuptools
+
+setuptools.setup(
+    name='lczero-tools',
+    version='0.1',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'dask',
+        'numpy',
+        'python-chess',
+    ],
+    extras_require={
+        'tf': [
+            'tensorflow',
+        ],
+        'torch': ['torch'],
+    },
+    setup_requires=[],
+    tests_require=[],
+)
